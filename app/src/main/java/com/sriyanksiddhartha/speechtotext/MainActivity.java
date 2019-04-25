b0d283c4b026f2a30df238a14848d42621add3c8
firashajbi <47852619+firashajbi@users.noreply.github.com>
2019-04-25 19:58:43 +0200

    Add files via upload

--- app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
@@ -1,51 +1,110 @@
-package com.sriyanksiddhartha.speechtotext;
+package com.example.casperapp;
 
+import android.content.pm.PackageManager;
 import android.content.Intent;
-import android.os.Bundle;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.Manifest;
+import android.provider.Settings;
+import android.net.Uri;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import android.widget.EditText;
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-	private TextView txvResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		txvResult = (TextView) findViewById(R.id.txvResult);
-	}
+        final EditText editText=(EditText) findViewById(R.id.editText);
+        final SpeechRecognizer mSpeechReconizer= SpeechRecognizer.createSpeechRecognizer(this);
 
-	public void getSpeechInput(View view) {
+        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        mSpeechReconizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
 
-		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+            }
 
-		if (intent.resolveActivity(getPackageManager()) != null) {
-			startActivityForResult(intent, 10);
-		} else {
-			Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
-		}
-	}
+            @Override
+            public void onBeginningOfSpeech() {
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
+            }
 
-		switch (requestCode) {
-			case 10:
-				if (resultCode == RESULT_OK && data != null) {
-					ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-					txvResult.setText(result.get(0));
-				}
-				break;
-		}
-	}
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> matches=bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if(matches != null)editText.setText(matches.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+        findViewById(R.id.button).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_UP:
+                        editText.setHint("Input will be here");
+                        break;
+                     case MotionEvent.ACTION_DOWN:
+                         mSpeechReconizer.startListening(mSpeechRecognizerIntent);
+                         editText.setText("");
+                         editText.setHint("Listening...");
+                         break;
+
+                }
+                return false;
+            }
+        });
+    }
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
 }
